b1d3652320abd327eb0c90dcc6b70d12adfc021d
Huziahmetov-Artur <36599960+Huziahmetov-Artur@users.noreply.github.com>
2019-04-26 11:26:24 +0300

    lab 1

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, StyleSheet} from 'react-native';
+
+import Game from './components/Game';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Game />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
---
+++ README.md
@@ -0,0 +1,21 @@
+# React Native Tic Tac Toe
+
+## Requirement
+
+Follow this: https://facebook.github.io/react-native/docs/getting-started.html
+
+## Run project locally:
+
+* Run `npm install`
+
+### For Android:
+
+* Run `react-native run-android`
+
+### For IOS:
+
+* Run `react-native run-ios`
+
+## Available on Expo Snack
+
+https://snack.expo.io/@numnine/tic-tac-toe
